4942145b5c1efcb2392494f9a70b3252bce3efc5
Jonah Snider <me@jonahsnider.ninja>
2019-11-07 20:51:49 -0800

    feat: add support for Spark motor controllers

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -6,31 +6,45 @@
 /*----------------------------------------------------------------------------*/
 package frc.robot;
 
+import frc.robot.util.controls.DriveJoystick;
+
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
-public class OI {
+public final class OI {
   //// CREATING BUTTONS
-// One type of button is a joystick button which is any button on a
-//// joystick.
-// You create one by telling it which joystick it's on and which button
-// number it is.
-// Joystick stick = new Joystick(port);
-// Button button = new JoystickButton(stick, buttonNumber);
-// There are a few additional built in buttons you can use. Additionally,
-// by subclassing Button you can create custom triggers and bind those to
-// commands the same as any other Button.
-//// TRIGGERING COMMANDS WITH BUTTONS
-// Once you have a button, it's trivial to bind it to a button in one of
-// three ways:
-// Start the command when the button is pressed and let it run the command
-// until it is finished as determined by it's isFinished method.
-// button.whenPressed(new ExampleCommand());
-// Run the command while the button is being held down and interrupt it once
-// the button is released.
-// button.whileHeld(new ExampleCommand());
-// Start the command when the button is released and let it run the command
-// until it is finished as determined by it's isFinished method.
-// button.whenReleased(new ExampleCommand());
+  // One type of button is a joystick button which is any button on a
+  //// joystick.
+  // You create one by telling it which joystick it's on and which button
+  // number it is.
+  // Joystick stick = new Joystick(port);
+  // Button button = new JoystickButton(stick, buttonNumber);
+  // There are a few additional built in buttons you can use. Additionally,
+  // by subclassing Button you can create custom triggers and bind those to
+  // commands the same as any other Button.
+  //// TRIGGERING COMMANDS WITH BUTTONS
+  // Once you have a button, it's trivial to bind it to a button in one of
+  // three ways:
+  // Start the command when the button is pressed and let it run the command
+  // until it is finished as determined by it's isFinished method.
+  // button.whenPressed(new ExampleCommand());
+  // Run the command while the button is being held down and interrupt it once
+  // the button is released.
+  // button.whileHeld(new ExampleCommand());
+  // Start the command when the button is released and let it run the command
+  // until it is finished as determined by it's isFinished method.
+  // button.whenReleased(new ExampleCommand());
+  private static final DriveJoystick driveJoystick = new DriveJoystick(RobotMap.joystick);
+
+  /**
+   * Scales a value to be more precise at lower values. Used for driving. Cubes
+   * the joystick value.
+   *
+   * @return scaled value
+   */
+  public static double scale(double value) {
+    // Cube the value
+    // return Math.pow(value, Config.joystickScalar);
+  }
 }
---
+++ src/main/java/frc/robot/subsystems/MotorSubsystem.java
@@ -0,0 +1,47 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import frc.robot.RobotMap;
+
+/**
+ * Motor subsystem.
+ * Makes robot go zoom.
+ */
+public class MotorSubsystem extends Subsystem {
+  /** The Spark motor controller for the front left side of the robot. */
+  private final Spark frontLeft = new Spark(RobotMap.frontLeftSparkPort);
+  /** The Spark motor controller for the front right side of the robot. */
+  private final Spark frontRight = new Spark(RobotMap.frontRightSparkPort);
+  /** The Spark motor controller for the back left side of the robot. */
+  private final Spark backLeft = new Spark(RobotMap.backLeftSparkPort);
+  /** The Spark motor controller for the back right side of the robot. */
+  private final Spark backRight = new Spark(RobotMap.backRightSparkPort);
+
+  /** Speed controller group for the motor controllers on the left. */
+  private final SpeedControllerGroup leftMotorControllers = new SpeedControllerGroup(frontLeft, backLeft);
+  /** Speed controller group for the motor controllers on the right. */
+  private final SpeedControllerGroup rightMotorControllers = new SpeedControllerGroup(frontRight, backRight);
+
+  /**
+   * The differential drivetrain for the motors.
+   * This is where most of the driving action is performed.
+   */
+  public final DifferentialDrive drive = new DifferentialDrive(leftMotorControllers, rightMotorControllers);
+
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+  // setDefaultCommand(new MySpecialCommand());
+  }
+}
