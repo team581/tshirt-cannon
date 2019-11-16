1c3883f678bfc57c1b3da6c951edd4b1d5deb250
Jonah Snider <jonah@jonah.pw>
2019-11-16 13:16:32 -0800

    style: format with Prettier
    Signed-off-by: Jonah Snider <jonah@jonah.pw>

--- src/main/java/frc/robot/subsystems/MotorSubsystem.java
+++ src/main/java/frc/robot/subsystems/MotorSubsystem.java
@@ -58,6 +58,6 @@ public class MotorSubsystem extends Subsystem {
   @Override
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
-    // setDefaultCommand(new MySpecialCommand());
+  // setDefaultCommand(new MySpecialCommand());
   }
 }
